0839f1ef13ad108ea72bedbced559d832b72bf75
ukapmck <uka1927.uu@gmail.com>
2021-05-24 20:18:39 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends ('layout/main');
+
+
+@section ('title', 'Web Ukaaaaa')
+
+
+@section ('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Kenageeeeeeeeeeeee</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+
+@extends ('layout/main');
+
+
+@section ('title', 'Web Ukaaaaa')
+
+
+@section ('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Hello Uka!</h1>
+        </div>
+    </div>
+</div>
+@endsection
